387af588a86796abb418627a1670e18af49e99d0
Mikhail Danshin <effectdopler0@gmail.com>
2021-08-29 11:58:17 +0300

    refactor: remove server
    prepare to migrate to firebase

--- packages/server/src/users/dto/create-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateUserDto {
-  @ApiProperty({ example: 'example@mail.com', description: 'Email' })
-  readonly email: string;
-  @ApiProperty({ example: 'password', description: 'Password' })
-  readonly password: string;
-}
