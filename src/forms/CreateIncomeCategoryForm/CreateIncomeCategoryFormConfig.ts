ad623a7c606ab948b1a2ee9364fbe2695d073245
Mikhail Danshin <effectdopler0@gmail.com>
2021-11-04 13:28:27 +0300

    refactor: remove unused form

--- src/forms/CreateIncomeCategoryForm/CreateIncomeCategoryFormConfig.ts
+++
@@ -1,9 +0,0 @@
-interface CreateIncomeCategoryFormConfig {
-  name: string
-}
-
-export const INITIAL_VALUES: CreateIncomeCategoryFormConfig = {
-  name: '',
-}
-
-export const FORM_TITLE = 'New Income Category'
